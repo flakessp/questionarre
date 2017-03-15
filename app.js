79b6e0dfd975da25f5ced12ca91aa1c71659ed1a
Sergei Panfilov <s.panfilov@maximatelecom.ru>
2017-03-15 18:20:09 +0300

    dokku shit

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express'),
-      app = express();
+      app = express(),
+      port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello Digital Ocean!')
 })
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Magic is happening on port 3000!')
-})
+});
